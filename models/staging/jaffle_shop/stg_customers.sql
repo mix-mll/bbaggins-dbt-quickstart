29a24e9192433811077189a1dee4d1cbfc5b2b33
mario loera <marioll@kth.se>
2024-03-16 11:28:52 -0700

    use employees in model

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,9 @@
 select
-    id as customer_id,
-    first_name,
-    last_name
+    c.id as customer_id,
+    c.first_name,
+    c.last_name,
+    e.employee_id is not null as is_employee
 
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source('jaffle_shop', 'customers') }} as c
+left join {{ ref('employees')}} as e
+    on c.id = e.customer_id
